19c319c70a8f1994cece00ee252115e7a733216b
Mohamed Salama Shakir <salamam301@yahoo.com>
2019-08-28 22:47:21 +0200

    Creating Contacts Vue Component

---
+++ resources/js/components/Contacts.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Contacts</h1>
+</template>
+
+<script>
+    export default {
+        data: function(){
+            return {}
+        },
+        mounted: function(){
+            console.log('Contacts Component loaded..');
+        }
+    }
+</script>
+
